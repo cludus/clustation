4f3a13d43bc4b98638f13f15ff67b6d9cd49b39f
Gilberto Vento <gilberto.vento@gmail.com>
2024-04-11 22:53:00 -0400

    adding python libraries

--- packages/sdks/python/inc.sh
+++ packages/sdks/python/inc.sh
@@ -3,7 +3,8 @@
 function install_python {
     sudo apt install build-essential libssl-dev zlib1g-dev libbz2-dev \
         libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev \
-        libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+        libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libcurl4-openssl-dev \
+        libssl-dev -y
 
     # rm -rf ~/.pyenv
     curl https://pyenv.run | bash
