05e75436d8391d9ffa4b3a6e1e7cca3be1b913db
Gilberto Vento <gilberto.vento@gmail.com>
2024-04-11 20:50:28 -0400

    removing jetbrains.

--- packages/gui/inc.sh
+++ packages/gui/inc.sh
@@ -16,19 +16,3 @@ function install_editors {
     sudo snap install code --classic
     sudo snap install atom --classic
 }
-
-function install_jetbrain_pro {
-    sudo snap install intellij-idea-ultimate --classic
-    sudo snap install rider --classic
-    sudo snap install goland --classic
-    sudo snap install pycharm-professional --classic
-    sudo snap install webstorm --classic
-    sudo snap install datagrip --classic
-    sudo snap install clion --classic
-    sudo snap install rubymine --classic
-}
-
-function install_jetbrain_comm {
-    sudo snap install pycharm-community --classic
-    sudo snap install intellij-idea-community --classic
-}
