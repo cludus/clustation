5071551de34715dd3149b6e85f38d3ca8b0ca30f
Gilberto José Vento Alvarez <gilberto.vento@gmail.com>
2024-04-15 20:28:28 -0400

    Update inc.sh

--- packages/vms/lxc/inc.sh
+++ packages/vms/lxc/inc.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 function install_lxc {
-    snap install lxd --channel=latest/stable
-    sudo adduser $USER lxd
-    lxc storage create local dir
-    lxc profile device add default root disk path=/ pool=local
+    sudo apt install lxc
 }
 
 function test_lxc {
     ct_name=lxc-test-ct
-    lxc launch ubuntu:22.04 $ct_name > /dev/null 2>&1 \
-        && lxc stop $ct_name > /dev/null 2>&1 \
-        && lxc delete $ct_name > /dev/null 2>&1
+    lxc-launch ubuntu:22.04 $ct_name > /dev/null 2>&1 \
+        && lxc-stop $ct_name > /dev/null 2>&1 \
+        && lxc-delete $ct_name > /dev/null 2>&1
     check_status $? "lxc"
 }
