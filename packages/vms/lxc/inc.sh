84f6578572cb71d168f13c3f41daffe2082abc4b
gilberto <gilberto.vento@gmail.com>
2024-04-17 13:50:40 -0400

    adding lxc config

--- packages/vms/lxc/inc.sh
+++ packages/vms/lxc/inc.sh
@@ -2,6 +2,18 @@
 
 function install_lxc {
     sudo apt install lxc -y
+
+    if ! test -f ~/.config/lxc/default.conf; then
+        user_id=$(grep $USER /etc/subuid | awk -F ":" '{print $2,$3}')
+        group_id=$(grep $USER /etc/subgid | awk -F ":" '{print $2,$3}')
+
+        mkdir -p ~/.config/lxc
+        echo "lxc.id_map = u 0 $user_id" > ~/.config/lxc/default.conf
+        echo "lxc.id_map = g 0 $group_id" >> ~/.config/lxc/default.conf
+        echo "lxc.network.type = veth" >> ~/.config/lxc/default.conf
+        echo "lxc.network.link = lxcbr0" >> ~/.config/lxc/default.conf
+        echo "$USER veth lxcbr0 2" | sudo tee -a /etc/lxc/lxc-usernet        
+    fi
 }
 
 function test_lxc {
