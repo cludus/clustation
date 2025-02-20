0eaf5050f6effeae278e73d26a79e4a34b71bf45
Gilberto Vento <gilberto.vento@gmail.com>
2025-02-20 11:53:29 -0500

    adding cudnn

--- packages/sdks/cpp/inc.sh
+++ packages/sdks/cpp/inc.sh
@@ -33,6 +33,12 @@ function install_cpp {
         rm -rf cuda-keyring_1.1-1_all.deb
         sudo apt-get update
         sudo apt-get -y install nvidia-cuda-toolkit
+
+        wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+        sudo dpkg -i cuda-keyring_1.1-1_all.deb
+        sudo apt-get update
+        sudo apt-get -y install cudnn
+        sudo apt-get -y install cudnn-cuda-12
     }
 
     do_install_cpp
